3bc2cf94eaf998ea547daa8d4e28733cae74c9ae
OJ Adeyemi <ojieadeyemi@gmail.com>
2024-08-05 00:49:00 -0400

    pylint minor update

--- usports_basketball/team_stats/__init__.py
+++ usports_basketball/team_stats/__init__.py
@@ -38,8 +38,7 @@ def __get_sport_identifier(gender: str) -> str:
         return "mbkb"
     if gender == "women":
         return "wbkb"
-    else:
-        raise ValueError("Gender must be 'men' or 'women'")
+    raise ValueError("Gender must be 'men' or 'women'")
 
 
 def __construct_urls(gender: str, season_option: str) -> tuple[str, str]:
